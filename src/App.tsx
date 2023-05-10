f31dca985e563b7a144ad6688da968888cdb7631
leshemo <81444861+leshemo@users.noreply.github.com>
2023-05-10 18:23:27 -0400

    will remember user inputted values after refresh

--- src/App.tsx
+++ src/App.tsx
@@ -17,22 +17,36 @@ function App() {
   );
 }
 
+//create a box that allows a user to pick a start and end station, and displays the number of bikes available at the start station and the number of docks available at the end station.
+//The start and end stations are picked using a dropdown, and the names and ids are retrieved from the BlueBikeGetStations function. The ids are passed to the BlueBikeGetTwoStationsStatus 
+//function, which then displays the number of bikes and docks available.
 function BlueBikeStationStatus() {
-  //create a box that allows a user to pick a start and end station, and displays the number of bikes available at the start station and the number of docks available at the end station.
-  //The start and end stations are picked using a dropdown, and the names and ids are retrieved from the BlueBikeGetStations function. The ids are passed to the BlueBikeGetTwoStationsStatus function, which then displays the number of bikes and docks available.
   const [available_bikes, setAvailableBikes] = useState([-1,-1]);
   const [loading, setLoading] = useState(true);
-  const [start_station, setStartStation] = useState(342);
-  const [end_station, setEndStation] = useState(12);
+  const [start_station, setStartStation] = useState(() => {
+    const saved = localStorage.getItem('start_station');
+    const initialValue = JSON.parse(saved!);
+    return initialValue || 342;
+  });
+
+  const [end_station, setEndStation] = useState(() => {
+    const saved = localStorage.getItem('end_station');
+    const initialValue = JSON.parse(saved!);
+    return initialValue || 12;
+  });
+
   const [stations, setStations] = useState([{name:'Fenway', id:342}, {name:'Ruggles', id:12}]);
 
   useEffect(() => {
-    BlueBikeGetTwoStationStatus(start_station, end_station).then(function(v) {
-      setAvailableBikes(v);
-      setLoading(false);
-    }).catch(console.error);
     BlueBikeGetStations().then(function(v) {
       setStations(v);
+      setLoading(false);
+    }).catch(console.error);
+    //adds the stations to the the local storage to keep if user refreshes the page
+    localStorage.setItem('start_station', JSON.stringify(start_station));
+    localStorage.setItem('end_station', JSON.stringify(end_station));
+    BlueBikeGetTwoStationStatus(start_station, end_station).then(function(v) {
+      setAvailableBikes(v);
       setLoading(false);
     }).catch(console.error);
 
@@ -72,12 +86,7 @@ function BlueBikeStationStatus() {
       </div>
     );
   }
-
-
 }
-
-
-
 
 
 export default App;

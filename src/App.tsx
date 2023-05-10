5813c43497dcec4c66f1f4d79d2626e4852984af
leshemo <81444861+leshemo@users.noreply.github.com>
2023-05-09 22:16:59 -0400

    added ability to pick start or end station ...
    from possible stations. The dropdown only seems to display the station id, not the name. More troubleshooting needed. added some random css, and split getting the stations and the status of two specific ones into two different api calls.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
-import {BlueBikeAPICall} from './api-call';
-import {useEffect,useState} from 'react'
+import {BlueBikeGetTwoStationStatus, BlueBikeGetStations} from './api-call';
+import {JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useEffect,useState} from 'react'
 
 
 function App() {
@@ -9,77 +9,73 @@ function App() {
       <header className="App-header">
         <p>
           Station Status:
+          <BlueBikeStationStatus />
         </p>
-        <BlueBikeButton />
+        
       </header>
     </div>
   );
 }
 
-function BlueBikeButton() {
-  //create a box that displays the number of bikes available at the default station and the number of docks available at the end station,
-  // using the BlueBikeAPICall function, and update the box every 5 seconds, and display the name of the start and end stations, and have a nice layout.
+function BlueBikeStationStatus() {
+  //create a box that allows a user to pick a start and end station, and displays the number of bikes available at the start station and the number of docks available at the end station.
+  //The start and end stations are picked using a dropdown, and the names and ids are retrieved from the BlueBikeGetStations function. The ids are passed to the BlueBikeGetTwoStationsStatus function, which then displays the number of bikes and docks available.
   const [available_bikes, setAvailableBikes] = useState([-1,-1]);
   const [loading, setLoading] = useState(true);
+  const [start_station, setStartStation] = useState(342);
+  const [end_station, setEndStation] = useState(12);
+  const [stations, setStations] = useState([{name:'Fenway', id:342}, {name:'Ruggles', id:12}]);
 
   useEffect(() => {
-    BlueBikeAPICall().then(function(v) {
+    BlueBikeGetTwoStationStatus(start_station, end_station).then(function(v) {
       setAvailableBikes(v);
       setLoading(false);
     }).catch(console.error);
+    BlueBikeGetStations().then(function(v) {
+      setStations(v);
+      setLoading(false);
+    }).catch(console.error);
+
+    const interval = setInterval(() => {
+      BlueBikeGetTwoStationStatus(start_station, end_station).then(function(v) {
+        setAvailableBikes(v);
+        setLoading(false);
+      }).catch(console.error);
+    }, 5000);
+    return () => clearInterval(interval);
   }
-  ,[]);
+  ,[start_station, end_station]);
+  
   if(loading) {
     return (
       <p> Loading... </p>
     );
   }
-  else {
+  else {    
     return (
-      <ul>
-        <li> Bikes Available at Fenway Station:{available_bikes[0]} </li>
-        <li> Docks Available at Ruggles Station:{available_bikes[1]} </li>
-      </ul>
-      
-      
+      <div>
+        <select value={start_station} onChange={e => setStartStation(+e.target.value)}>
+          {stations.map((station: any) => (
+            <option value={station.id}>{station.id}</option>
+          ))}
+        </select>
+        <select value={end_station} onChange={e => setEndStation(+e.target.value)}>
+          {stations.map((station: any) => (
+            <option value={station.id}>{station.id}</option>
+          ))}
+        </select>
+        <div> </div>
+        <p> {available_bikes[0]} bikes available at {stations.find(x => x.id === start_station)?.name} </p>
+        <p> {available_bikes[1]} docks available at {stations.find(x => x.id === end_station)?.name} </p>
+      </div>
     );
   }
+
+
 }
 
 
 
+
+
 export default App;
-
-
-
-
-
-
-
-
-  // // const [available_bikes, setAvailableBikes] = useState([-1,-1]);
-  // const [available_bikes, setAvailableBikes] = useState([-1,-1]);
-  // const [loading, setLoading] = useState(true);
-  
-  // useEffect(() => {
-  //   BlueBikeAPICall().then(function(v) {
-  //     setAvailableBikes(v);
-  //     setLoading(false);
-  //   }).catch(console.error);
-  // }
-  // ,[]);
-
-  // if(loading) {
-  //   return (
-  //     <button> Loading... </button>
-  //   );
-  // }
-  // else {
-  //   return (
-  //     <p> Bikes Available at Fenway Station:{available_bikes[0]} {'\n'}
-  //     Docks Available at Ruggles Station:{available_bikes[1]}
-  //     </p>
-      
-      
-  //   );
-  // }
--- src/api-call.tsx
+++ src/api-call.tsx
@@ -1,51 +1,45 @@
 import axios from 'axios';
+import { stat } from 'fs/promises';
 
-export async function BlueBikeAPICall(start_station_id:number = 342, end_station_id:number = 12) {
+export async function BlueBikeGetTwoStationStatus(start_station_id:number = 342, end_station_id:number = 12) {
     
     const result = await axios.get('https://gbfs.bluebikes.com/gbfs/en/station_status.json');
     const stations = result.data.data.stations
     let start_station;
     let end_station;
 
+
     for (var station of stations) {
+
         if (station.station_id == start_station_id) {
             start_station = station
-        } else if (station.station_id == end_station_id) {
+        }  
+        if (station.station_id == end_station_id) {
             end_station = station
         }
             
     }
+    
     const available:number[] = [start_station.num_bikes_available, end_station.num_docks_available]
 
     return available;
     
+    
 }
 
-//create a new function that does what the below function does but avoid returning a promise
-// export function BlueBikeAPICall(start_station_id:number = 342, end_station_id:number = 12) {
-//     axios.get('https://gbfs.bluebikes.com/gbfs/en/station_status.json')
-//     .then(function (response) {
-//         const stations = response.data.data.stations
-//         let start_station;
-//         let end_station;
+export async function BlueBikeGetStations() {
+    
+    const result = await axios.get('https://gbfs.bluebikes.com/gbfs/en/station_status.json');
+    const stations = result.data.data.stations
 
-//         for (var station of stations) {
-//             if (station.station_id == start_station_id) {
-//                 start_station = station
-//             } else if (station.station_id == end_station_id) {
-//                 end_station = station
-//             }
+    let station_names_and_ids: {name:string, id:number}[] = []
+    for (var station of stations) {
+        station_names_and_ids.push({name:station.name, id:station.station_id})
+            
+    }
+    
+    return station_names_and_ids;
 
-//         }
-//         const available:number[] = [start_station.num_bikes_available, end_station.num_docks_available]
+    
+}
 
-//         return available;
-
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     })
-//     .then(function () {
-//         // always executed
-//     });
-// }
